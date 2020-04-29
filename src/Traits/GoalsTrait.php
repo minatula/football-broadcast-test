b5266f7250c59e9115b5b39b74981298b662d2a1
minatula <minatula.shakhbazov@gmail.com>
2020-04-29 18:28:35 +0300

    PHPDocs deleted from GoalsTrait

--- src/Traits/GoalsTrait.php
+++ src/Traits/GoalsTrait.php
@@ -4,22 +4,13 @@ namespace App\Traits;
 
 trait GoalsTrait
 {
-	/**
-	 * @var int
-	 */
 	private int $goals = 0;
 
-	/**
-	 * @return void
-	 */
 	public function addGoal(): void
 	{
 		$this->goals += 1;
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getGoals(): int
 	{
 		return $this->goals;
