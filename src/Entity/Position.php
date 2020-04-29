a87accdf627f629e7dea601e26caff9a3da8b0b7
minatula <minatula.shakhbazov@gmail.com>
2020-04-29 18:29:27 +0300

    PHPDocs deleted from Position model

--- src/Entity/Position.php
+++ src/Entity/Position.php
@@ -4,46 +4,26 @@ namespace App\Entity;
 
 class Position
 {
-	/**
-	 * @var string
-	 */
 	private string $name;
 
-	/**
-	 * @var int
-	 */
 	private int $totalTime;
 
-	/**
-	 * @param string $name
-	 */
 	public function __construct(string $name)
 	{
 		$this->name = $name;
 		$this->totalTime = 0;
 	}
 
-	/**
-	 * @return string
-	 */
 	public function getName(): string
 	{
 		return $this->name;
 	}
 
-	/**
-	 * @return int
-	 */
 	public function getTotalTime(): int
 	{
 		return $this->totalTime;
 	}
 
-	/**
-	 * @param $minutes
-	 *
-	 * @return void
-	 */
 	public function increaseTotalTime($minutes): void
 	{
 		$this->totalTime += $minutes;
